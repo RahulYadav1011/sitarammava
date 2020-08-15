a0a0978fc0527631bf302a4903aaae4a9fb9cc46
RahulYadav1011 <52218278+RahulYadav1011@users.noreply.github.com>
2020-08-15 22:15:56 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,9 +1,16 @@
 <?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['message'];
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$message=$_REQUEST['message'];
+if(empty($name) || empty($email) ||empty($message))
+{
+     echo "please fill all the details";
+}
+else
+{
+     
 mail("rahulyadavyadav306@gmail.com","QUERY MESSAGE",$message,"From: $name <$email>");
-echoi"<script>alert('mail sended');
+echo "<script>alert('mail sended');
      window.history.log(-1);
 </script>";
 ?>
