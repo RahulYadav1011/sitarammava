33f86db4527d7d3e212482ac6b14cc9cb800c852
RahulYadav1011 <52218278+RahulYadav1011@users.noreply.github.com>
2020-08-15 17:11:42 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,4 +3,5 @@ $name=$_POST['name'];
 $email=$_POST['email'];
 $message=$_POST['message'];
 mail("rahulyadavyadav306@gmail.com","QUERY MESSAGE",$message,"From: $name<$email>");
+echo <script>alert('mail sended');</script>;
 ?>
